2f8b5f2a08e8668b9713676c5f6994555957b876
Makiesse <makiesse.dev@gmail.com>
2024-12-10 18:45:02 +0100

    uppercase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <section className="flex h-full flex-col items-center justify-center gap-6">
-      <h1 className="text-violet-600 text-8xl uppercase">Devao.tech</h1>
+      <h1 className="text-violet-600 text-8xl uppercase">DEVAO.TECH</h1>
       <h2 className="text-violet-100 text-2xl">building...</h2>
     </section>
   );
